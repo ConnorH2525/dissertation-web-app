597fdd78571ec1d3371560c06f9a9a76581a55f1
Connor Harrison <connorjake2525@gmail.com>
2021-02-24 17:39:00 +0000

    Update Navbar.js

--- dissertation-web-app/src/components/layout/Navbar.js
+++ dissertation-web-app/src/components/layout/Navbar.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Navbar } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import { useAuth } from "../../contexts/AuthContext"
 import SignedInLinks from "./SignedInLinks"
 import SignedOutLinks from "./SignedOutLinks"
 
 const TopNavbar = () => {
+    const { currentUser } = useAuth()
+    const links = currentUser ? <SignedInLinks /> : <SignedOutLinks />
+
     return (
     <Navbar bg="light" expand="sn">
             <Navbar.Brand as={Link} to ="/">
                 Picture Frame App
             </Navbar.Brand>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {links}
         </Navbar>
     )
 }
