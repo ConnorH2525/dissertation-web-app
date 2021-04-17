6c4d3eda94f51975e899ea422ae4d5bdf5087c23
Connor Harrison <connorjake2525@gmail.com>
2021-04-17 18:04:30 +0100

    Update Viewer.js

--- dissertation-web-app/src/components/viewer/Viewer.js
+++ dissertation-web-app/src/components/viewer/Viewer.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useGroup } from "../../hooks/useGroup"
 import { useParams, useLocation } from "react-router-dom"
 import CenteredContainer from '../authentication/CenteredContainer'
+import firebase, { database } from "../../firebase"
+import "firebase/firestore"
 
 const Viewer = () => {
 
@@ -13,24 +15,32 @@ const Viewer = () => {
             <div style={{height: "100%", backgroundColor: "black"}}>
                 {childFiles.length > 0 && (
                     <div>
-                    {childFiles.map(childFile => (
+                    {childFiles.map(async (childFile, index) => (
                         <div 
                             key={childFile.id}
                         >
                             {!childFile.isText && 
-                                <img src={childFile.url}
-                                alt="example"
-                                width="100%"/>
+                                <div style={{backgroundColor: "black"}}>
+                                    <img src={childFile.url}
+                                    alt="example"
+                                    width="100%"/>
+                                </div>
                             }
                             {childFile.isText && 
+                                <div style={{backgroundColor: "white"}}>
                                 <CenteredContainer>
                                     <h1>{childFile.text}</h1>
                                 </CenteredContainer>
-                            }
+                                </div>
+                            } 
+                            {<h1>{firebase.firestore().collection("users")
+                                .doc(childFile.userId)
+                                .get(username)}</h1>}
                         </div>
                     ))}
                     </div>
                 )}
+
             </div>
             
     )
